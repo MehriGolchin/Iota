9e3f2912b0dca5118d6e42c3ca49e101bc669a0d
MehriGolchin <mehri.GOLCHIN-KHARAZI@etu.univ-amu.fr>
2019-04-10 16:44:58 +0200

    Move gutter to Column (KAN-21)

--- src/components/grid/grid.stories.tsx
+++ src/components/grid/grid.stories.tsx
@@ -38,37 +38,37 @@ storiesOf('Grid System', module)
   ))
   .add('Guttered Grid', () => (
     <div style={containerStyle}>
-        <Row style={rowStyle} gutter={15}>
-            <Column style={colStyle}><div>Col 1</div></Column>
-            <Column style={colStyle}>Col 2</Column>
-            <Column style={colStyle}>Col 3</Column>
+        <Row style={rowStyle}>
+            <Column style={colStyle} gutter={15}>Col 1</Column>
+            <Column style={colStyle} gutter={15}>Col 2</Column>
+            <Column style={colStyle} gutter={15}>Col 3</Column>
         </Row>
     </div>
   ))
   .add('Different Column Size', () => (
     <div style={containerStyle}>
-        <Row style={rowStyle} gutter={15} cover>
-            <Column style={colStyle} grow={2}><div>Col 1</div></Column>
-            <Column style={colStyle}>Col 2</Column>
-            <Column style={colStyle}>Col 3</Column>
+        <Row style={rowStyle} cover>
+            <Column style={colStyle} grow={2} gutter={15}>Col 1</Column>
+            <Column style={colStyle} gutter={15}>Col 2</Column>
+            <Column style={colStyle} gutter={15}>Col 3</Column>
         </Row>
     </div>
   ))
   .add('Vertical Grid', () => (
     <div style={containerStyle}>
-        <Row style={rowStyle} gutter={15} vertical cover>
-            <Column style={colStyle} grow={2}><div>Col 1</div></Column>
-            <Column style={colStyle}>Col 2</Column>
-            <Column style={colStyle}>Col 3</Column>
+        <Row style={rowStyle} vertical cover>
+            <Column style={colStyle} grow={2} gutter={15}>Col 1</Column>
+            <Column style={colStyle} gutter={15}>Col 2</Column>
+            <Column style={colStyle} gutter={15}>Col 3</Column>
         </Row>
     </div>
   ))
   .add('Justify Content', () => (
     <div style={containerStyle}>
-        <Row style={rowStyle} gutter={15} vertical cover>
-            <Column style={colStyle}>Col 1</Column>
-            <Column style={colStyle} justify='center'>Col 2</Column>
-            <Column style={colStyle} justify='right'>Col 3</Column>
+        <Row style={rowStyle} vertical cover>
+            <Column style={colStyle} gutter={15}>Col 1</Column>
+            <Column style={colStyle} gutter={15} justify='center'>Col 2</Column>
+            <Column style={colStyle} gutter={15} justify='right'>Col 3</Column>
         </Row>
     </div>
   ));
