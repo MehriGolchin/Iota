951c1e74bbdfa1d1be7692d872eda3d676dc9422
MehriGolchin <mehri.GOLCHIN-KHARAZI@etu.univ-amu.fr>
2019-04-10 16:44:58 +0200

    Introduce Grid System (KAN-17)

--- src/components/grid/grid.styles.ts
+++ src/components/grid/grid.styles.ts
@@ -3,29 +3,28 @@ import styled, { css } from 'styled-components';
 export interface RowProps {
     readonly vertical?: boolean;
     readonly cover?: boolean;
-    readonly gutter?: number;
+    
 }
 
 export interface ColumnProps {
     readonly grow?: number;
     readonly justify?: string;
+    readonly gutter?: number;
 }
 
 export const Row = styled('div') <RowProps>`
     display: flex;
     align-items: stretch;
     flex-direction: ${(props) => props.vertical ? 'column' : 'row'};
-    ${(props) => props.cover && css`
+    ${(props) => props.cover && css` 
         height: 100%;
-    `}
-    ${(props) => props.gutter && css`
-        & > div {
-            margin: ${props.gutter}px;
-        }
     `}
 `;
 
 export const Column = styled('div') <ColumnProps>`
     text-align: ${(props) => props.justify};
     flex: ${(props) => props.grow || 1};
+    ${(props) => props.gutter && css`
+        margin: ${props.gutter}px;
+    `}
 `;
