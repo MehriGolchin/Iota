cd4534bdaf156a50806d29b36a60b155a714de14
MehriGolchin <mehri.GOLCHIN-KHARAZI@etu.univ-amu.fr>
2019-04-10 21:29:47 +0200

    Configure webpack to bundle iota as a library (KAN-20)
    - React and styled-components moved from dependencies to peerDependencies.
    - Compiler target decreased to es5.
    - Source-map generator enabled.
    - Introduced a webpack environment for storybook.

--- .webpack/dev.config.js
+++ .webpack/dev.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    mode: "development"
+    mode: "development",
+    devtool: 'source-map',
+    externals: {
+        'react': 'react',
+        'styled-components': {
+            commonjs: "styled-components",
+            commonjs2: "styled-components",
+            amd: "styled-components",
+        },
+    }
 };
